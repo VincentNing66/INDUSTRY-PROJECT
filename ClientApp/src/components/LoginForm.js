5c5f34397e64900cb54944a6c6405008f43604fb
Nadia Wesley <nwesley49@gmail.com>
2019-10-01 22:01:54 +1300

    Added css folder & file, linked LoginForm to App.js

--- ClientApp/src/components/LoginForm.js
+++ ClientApp/src/components/LoginForm.js
@@ -1,26 +1,37 @@
 import React, { Component } from 'react';
+import "./css/styles.css";
 
-export class Home extends Component {
-  static displayName = Home.name;
+export class LoginForm extends Component {
+    static displayName = LoginForm.name;
 
-  render () {
-    return (
-      <div>
-        <h1>Hello, world!</h1>
-        <p>Welcome to your new single-page application, built with:</p>
-        <ul>
-          <li><a href='https://get.asp.net/'>ASP.NET Core</a> and <a href='https://msdn.microsoft.com/en-us/library/67ef8sbd.aspx'>C#</a> for cross-platform server-side code</li>
-          <li><a href='https://facebook.github.io/react/'>React</a> for client-side code</li>
-          <li><a href='http://getbootstrap.com/'>Bootstrap</a> for layout and styling</li>
-        </ul>
-        <p>To help you get started, we have also set up:</p>
-        <ul>
-          <li><strong>Client-side navigation</strong>. For example, click <em>Counter</em> then <em>Back</em> to return here.</li>
-          <li><strong>Development server integration</strong>. In development mode, the development server from <code>create-react-app</code> runs in the background automatically, so your client-side resources are dynamically built on demand and the page refreshes when you modify any file.</li>
-          <li><strong>Efficient production builds</strong>. In production mode, development-time features are disabled, and your <code>dotnet publish</code> configuration produces minified, efficiently bundled JavaScript files.</li>
-        </ul>
-        <p>The <code>ClientApp</code> subdirectory is a standard React application based on the <code>create-react-app</code> template. If you open a command prompt in that directory, you can run <code>npm</code> commands such as <code>npm test</code> or <code>npm install</code>.</p>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <h1>Log Into Dashboard</h1>
+                <form action="action_page.php" method="post">
+                    <div>
+                        <img src={require('./img/willingwebicon.png')} alt="Willing Web" className='image' />
+                    </div>
+                    <div class="container">
+                        <label for="username"><b>Username</b></label><br></br>
+                        <input className="inputbox" type="text" placeholder="Enter Username" name="username" required></input>
+                        <br></br>
+                        <label for="password"><b>Password</b></label><br></br>
+                        <input className="inputbox" type="password" placeholder="Enter Password" name="password" required ></input>
+                        <br></br>
+                        <br></br>
+                        <button className="button" type="submit">Login</button>
+                        <br></br>
+                        <label for="remember"><b>Remember Me?</b></label><br></br>
+                        <input type="checkbox" checked="checked" name="remember"></input>
+                    </div>
+                    <div class="container">
+                        <button type="button" className="cancelbtn">Cancel</button>
+                        <br></br><label><a href="#"><b>Forgot Password?</b></a></label><br></br>
+                    </div>
+                </form>
+            </div>
+        );
+    }
 }
+// LoginForm can be accessed by URL https://localhost:44313/LoginForm
