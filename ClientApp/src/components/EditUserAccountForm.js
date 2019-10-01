18c14e0357bfc181e104df9d5936c3d191798f99
Nadia Wesley <nwesley49@gmail.com>
2019-10-02 00:03:36 +1300

    Added labels to each input box

--- ClientApp/src/components/EditUserAccountForm.js
+++ ClientApp/src/components/EditUserAccountForm.js
@@ -11,22 +11,30 @@ export class EditUserAccountForm extends Component {
                 <h1 className="center-text">Edit User Account</h1>
                 <hr></hr>
                 <form action="action_page.php" method="post">
+                    <label for="username"><b>Username:</b></label>
                     <input className="form-control" type="text" placeholder="Enter Username" name="username" id="username" required ></input>
                     <br></br>
+                    <label for="firstname"><b>First Name:</b></label><br></br>
                     <input className="form-control" type="text" placeholder="Enter First Name" name="firstname" id="firstname" required ></input>
                     <br></br>
+                    <label for="lastname"><b>Last Name:</b></label><br></br>
                     <input className="form-control" type="text" placeholder="Enter Last Name" name="lastname" id="lastname" required ></input>
                     <br></br>
+                    <label for="address"><b>Address:</b></label><br></br>
                     <input className="form-control" type="text" placeholder="Enter Address" name="address" id="address" required ></input>
                     <br></br>
+                    <label for="emailAddress"><b>Email Address:</b></label><br></br>
                     <input className="form-control" type="text" placeholder="Enter Email Address" name="emailAddress" id="emailAddress" required ></input>
                     <br></br>
+                    <label for="permission"><b>Permission</b></label><br></br>
                     <input className="form-control" type="text" placeholder="Permission" name="permission" id="permission" required ></input>
                     <br></br>
                     <hr></hr>
                     <br></br>
+                    <label for="updatePassword"><b>Password:</b></label><br></br>
                     <input className="form-control" type="password" placeholder="Update Password" name="permission" id="updatePassword" required ></input>
                     <br></br>
+                    <label for="confirmPassword"><b>Confirm Password:</b></label><br></br>
                     <input className="form-control" type="password" placeholder="Confirm New Password" name="confirmPassword" id="confirmPassword" required ></input>
                     <br></br>
                     <div className="justify-content-center row">
