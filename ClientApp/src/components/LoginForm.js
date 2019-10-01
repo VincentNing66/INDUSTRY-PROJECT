3cbfae829a744e39de3ad44bcbf8a9126435cdd2
Travis Wylie <traviswylie58@gmail.com>
2019-10-01 23:09:42 +1300

    login screen improvements
    Inversed logo
    changed button style
    centered content
    aligned remember and forgot password

--- ClientApp/src/components/LoginForm.js
+++ ClientApp/src/components/LoginForm.js
@@ -6,13 +6,13 @@ export class LoginForm extends Component {
 
     render() {
         return (
-            <div>
+            <div className="container justify-content-center col-md-12 center">
                 <h1>Log Into Dashboard</h1>
                 <form action="action_page.php" method="post">
                     <div>
-                        <img src={require('./img/willingwebicon.png')} alt="Willing Web" className='image' />
+                        <img src={require('./img/willingwebinverse.png')} alt="Willing Web" className='image' />
                     </div>
-                    <div class="container">
+                    <div class="container justify-content-center">
                         <label for="username"><b>Username</b></label><br></br>
                         <input className="inputbox" type="text" placeholder="Enter Username" name="username" required></input>
                         <br></br>
@@ -20,14 +20,20 @@ export class LoginForm extends Component {
                         <input className="inputbox" type="password" placeholder="Enter Password" name="password" required ></input>
                         <br></br>
                         <br></br>
-                        <button className="button" type="submit">Login</button>
+                        <button className="btn btn-primary button" type="button">Login</button>
                         <br></br>
-                        <label for="remember"><b>Remember Me?</b></label><br></br>
-                        <input type="checkbox" checked="checked" name="remember"></input>
-                    </div>
-                    <div class="container">
-                        <button type="button" className="cancelbtn">Cancel</button>
-                        <br></br><label><a href="#"><b>Forgot Password?</b></a></label><br></br>
+                        <br></br>
+                        <div className="row justify-content-center">
+                            <div className="col-md-6">
+                                <div className="justify-content-center">
+                                    <input type="checkbox" checked="checked" name="remember"></input>
+                                    <label for="remember" className="padLeft"><b>Remember Me?</b></label><br></br>
+                                </div>
+                            </div>
+                            <div className="col-md-6">
+                                <label><a href="#"><b>Forgot Password?</b></a></label><br></br>
+                            </div>
+                        </div>
                     </div>
                 </form>
             </div>
