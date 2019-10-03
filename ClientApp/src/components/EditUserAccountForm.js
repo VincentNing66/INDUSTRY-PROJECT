a32e48f0c03b089939364347b52b549fd0776a2a
Nadia Wesley <nwesley49@gmail.com>
2019-10-03 22:22:17 +1300

    Working on backend for EditUserAccount. Not finished.

--- ClientApp/src/components/EditUserAccountForm.js
+++ ClientApp/src/components/EditUserAccountForm.js
@@ -4,6 +4,44 @@ import './css/style.css';
 export class EditUserAccountForm extends Component {
     static displayName = EditUserAccountForm.name;
 
+    //This method is triggered when the "save" button is clicked. It sends data from the form to the database.
+    updateUser() {
+        var uname = document.getElementById("username").value;
+        var fname = document.getElementById("firstname").value;
+        var lname = document.getElementById("lastname").value;
+        var address = document.getElementById("address").value;
+        var email = document.getElementById("emailAddress").value;
+        var pword = document.getElementById("updatePassword").value;
+        var confirm_pword = document.getElementById("confirmPassword").value;
+        //If the password and confirm password fields are the same
+        if (pword === confirm_pword) {
+            var JsonString = `{
+              "Username": "`+ uname + `",
+              "FirstName": "`+ fname + `",
+              "LastName": "`+ lname + `",
+              "Address": "`+ address + `",
+              "EmailAddress":"` + email + `",
+              "Password": "`+ pword + `",
+              "PermissionsId": 1
+            }`;
+            try {
+                fetch('api/SampleData/updateUser', {
+                    method: 'POST',
+                    body: JsonString,
+                    headers: {
+                        'Content-Type': 'application/json'
+                    }
+                });
+            }
+            catch (Exception) {
+                //Something went wrong with the API request
+                var random = 0;
+            }
+        }
+        else {
+            window.alert("Unable to update user account. The entered password and confirm password does not match. Try again.");
+        }
+    }
   render () {
     return (
         <div className="container col-lg-12 justify-content-center">
@@ -11,30 +49,30 @@ export class EditUserAccountForm extends Component {
                 <h1 className="center-text">Edit User Account</h1>
                 <hr></hr>
                 <form action="action_page.php" method="post">
-                    <label for="username"><b>Username:</b></label>
+                    <label htmlFor="username"><b>Username:</b></label>
                     <input className="form-control" type="text" placeholder="Enter Username" name="username" id="username" required ></input>
                     <br></br>
-                    <label for="firstname"><b>First Name:</b></label><br></br>
-                    <input className="form-control" type="text" placeholder="Enter First Name" name="firstname" id="firstname" required ></input>
+                    <label htmlFor="firstname"><b>First Name:</b></label><br></br>
+                    <input className="form-control" type="text" placeholder="Enter First Name" name="firstname" id="firstname" ></input>
                     <br></br>
-                    <label for="lastname"><b>Last Name:</b></label><br></br>
-                    <input className="form-control" type="text" placeholder="Enter Last Name" name="lastname" id="lastname" required ></input>
+                    <label htmlFor="lastname"><b>Last Name:</b></label><br></br>
+                    <input className="form-control" type="text" placeholder="Enter Last Name" name="lastname" id="lastname" ></input>
                     <br></br>
-                    <label for="address"><b>Address:</b></label><br></br>
-                    <input className="form-control" type="text" placeholder="Enter Address" name="address" id="address" required ></input>
+                    <label htmlFor="address"><b>Address:</b></label><br></br>
+                    <input className="form-control" type="text" placeholder="Enter Address" name="address" id="address" ></input>
                     <br></br>
-                    <label for="emailAddress"><b>Email Address:</b></label><br></br>
+                    <label htmlFor="emailAddress"><b>Email Address:</b></label><br></br>
                     <input className="form-control" type="text" placeholder="Enter Email Address" name="emailAddress" id="emailAddress" required ></input>
                     <br></br>
-                    <label for="permission"><b>Permission</b></label><br></br>
-                    <input className="form-control" type="text" placeholder="Permission" name="permission" id="permission" required ></input>
+                    <label htmlFor="permission"><b>Select Permission:</b></label><br></br>
+                    <select name="permissions" id="permissions" options={this.state.perms} onChange={(values) => this.setValues(values)} placeholder="Permission" className="form-control" required></select>
                     <br></br>
                     <hr></hr>
                     <br></br>
-                    <label for="updatePassword"><b>Password:</b></label><br></br>
+                    <label htmlFor="updatePassword"><b>Password:</b></label><br></br>
                     <input className="form-control" type="password" placeholder="Update Password" name="permission" id="updatePassword" required ></input>
                     <br></br>
-                    <label for="confirmPassword"><b>Confirm Password:</b></label><br></br>
+                    <label htmlFor="confirmPassword"><b>Confirm Password:</b></label><br></br>
                     <input className="form-control" type="password" placeholder="Confirm New Password" name="confirmPassword" id="confirmPassword" required ></input>
                     <br></br>
                     <div className="justify-content-center row">
