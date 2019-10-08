f34fc69fc21c977ac884473737c822f49f69df9b
Vincent Ning <Vincent@Vincents-MacBook-Pro.local>
2019-10-08 15:16:10 +1300

    Done

--- ClientApp/src/components/ManageUserAccount.js
+++ ClientApp/src/components/ManageUserAccount.js
@@ -1,5 +1,26 @@
 import React, { Component } from 'react';
 
+var fakeName = 'Bob';
+function deleteUser(){
+  window.confirm('Are you sure you want to delete '+ xxxx() +' ?')? alert("User deleted") : alert("Request canceled")
+}
+
+function xxxx(){
+
+  fakeName = document.getElementById("userSearch").value;
+
+  return fakeName;
+}
+function searchUser(){
+  if (document.getElementById("userSearch").value==fakeName){
+    alert('User Not Found!')
+  } else if(document.getElementById("userSearch").value=='John'){
+    alert('User Not Found!');
+  } else{
+    alert('User Found')
+    fakeName = document.getElementById("userSearch").value;
+  }
+}
 export class ManageUserAccount extends Component {
     static displayName = ManageUserAccount.name;
     constructor(props) {
@@ -26,8 +47,8 @@ export class ManageUserAccount extends Component {
                   <div className="m2">
                     <span className="col-lg-6">Search For User : </span>
                   </div>
-                  <input class="inline m2 form-control col-lg-5" type="text" placeholder="Search" aria-label="Search"></input>
-                  <button type="button" class="flowRight m2 inline col-lg-5 btn btn-primary">Search For User</button>
+                  <input id="userSearch" class="inline m2 form-control col-lg-5" type="text" placeholder="Search" aria-label="Search"></input>
+                  <button type="button" class="flowRight m2 inline col-lg-5 btn btn-primary" onClick={searchUser}>Search For User</button>
                   <hr/>
                   <div>
                     <div className="inline col-lg-6">
@@ -56,9 +77,9 @@ export class ManageUserAccount extends Component {
                       <div class="form-group">
                         {permissionContents}
                       </div>
-                      <button className="dashButton ol-lg-5 btn btn-primary">Update User Permission</button>
-                      <button className="dashButton ol-lg-5 btn btn-primary">Unlock User Account</button>
-                      <button className="dashButton ol-lg-5 btn btn-primary">Delete User Account</button>
+                      <button className="dashButton ol-lg-5 btn btn-primary" onClick={selectUser}>Update User Permission</button>
+                      <button className="dashButton ol-lg-5 btn btn-primary" onClick={unlockUser}>Unlock User Account</button>
+                      <button className="dashButton ol-lg-5 btn btn-primary" onClick={deleteUser}>Delete User Account</button>
                       <button className="dashButton ol-lg-5 btn btn-primary" onClick={goToCreate}>Create New User Account</button>
                     </div>
                   </div>
@@ -82,4 +103,10 @@ export class ManageUserAccount extends Component {
 
       
 }
-function goToCreate(){window.location.pathname = "/createUserAccount"}
+function goToCreate(){window.location.pathname = "/creatUserAccount"}
+
+
+
+function selectUser(){alert('User Updated For User '+ xxxx() +'!')}
+
+function unlockUser(){alert('User Unlocke ' + xxxx() +'!')}
